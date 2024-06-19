90a3afc4a873bef959c7cc605ee9fec43f37380c
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-06-19 23:25:57 +0200

    Player Controls Update (End Session 5)

--- src/util/player.ts
+++ src/util/player.ts
@@ -1,5 +1,7 @@
 import k from "@/kaplay";
-import { GameObj } from "kaplay";
+import { EventController, GameObj } from "kaplay";
+import { trueMousePos } from "./mouse";
+import { getMagnitute } from "./math";
 
 function createPlayer(x: number, y: number, parent: GameObj) {
   const player = parent.add([
@@ -7,7 +9,10 @@ function createPlayer(x: number, y: number, parent: GameObj) {
     k.color("#2B313F"),
     k.pos(x, y),
     k.area(),
-    k.state("idle", ["rolling", "flying", "finished"]),
+    k.state("idle", ["idle", "dragging", "rolling", "flying", "finished"]),
+    {
+      velocity: k.vec2(0, 0),
+    },
   ]);
 
   player.add([k.circle(10), k.color("#F6F8FB")]);
@@ -15,31 +20,68 @@ function createPlayer(x: number, y: number, parent: GameObj) {
   const arrowHolder = player.add([
     k.pos(0, 0),
     k.anchor("center"),
-    k.area(),
     k.rotate(0),
   ]);
 
-  const arrows = arrowHolder.add([k.sprite("arrow0"), k.pos(0, 32)]);
-
-  let isDragging = false;
+  const arrows = arrowHolder.add([
+    k.sprite("arrows"),
+    k.anchor("bot"),
+    k.pos(0, -32),
+    k.opacity(0),
+  ]);
 
   player.onMouseDown(() => {
     if (player.isHovering()) {
-      isDragging = true;
+      player.enterState("dragging");
+      arrows.opacity = 1;
     }
   });
 
   player.onMouseRelease(() => {
-    if (isDragging) {
-      isDragging = false;
-      player.use("rolling");
+    console.log(player.state);
+    if (player.state === "dragging") {
+      arrows.opacity = 0;
+
+      const velocityThreshold = 28;
+      const vel = player.worldPos().sub(trueMousePos()).scale(0.2);
+
+      let mag = getMagnitute(vel);
+
+      let factor = Math.min(mag, velocityThreshold) / mag;
+
+      console.log(mag, factor, k.dt());
+
+      player.velocity = vel.scale(factor);
+
+      player.enterState("rolling");
     }
   });
 
+  let loop: EventController;
+
+  function stopLoop() {
+    loop.cancel();
+  }
+
+  player.onStateEnter("rolling", () => {
+    loop = k.loop(0.025, () => {
+      player.moveBy(player.velocity);
+
+      player.velocity = player.velocity.scale(0.95);
+
+      if (getMagnitute(player.velocity) < 0.4) {
+        console.log("Stopped rolling");
+        player.enterState("idle");
+        stopLoop();
+      }
+    });
+  });
+
   player.onUpdate(() => {
-    if (isDragging) {
-      arrowHolder.angle = k.mousePos().angle(arrowHolder.pos) - 90;
-      let dist = k.mousePos().dist(player.pos);
+    if (player.state === "dragging") {
+      let mousePos = trueMousePos();
+      arrowHolder.angle = mousePos.angle(player.worldPos()) - 90;
+      let dist = mousePos.dist(player.worldPos());
       if (dist > 100) {
         arrows.play("3");
       } else if (dist > 50) {
@@ -47,6 +89,9 @@ function createPlayer(x: number, y: number, parent: GameObj) {
       } else {
         arrows.play("1");
       }
+    }
+
+    if (player.state === "rolling") {
     }
   });
 }
