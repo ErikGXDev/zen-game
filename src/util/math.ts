965e2649a189ca30d359be01102f9880edb39408
ErikGXDev <54033728+ErikGXDev@users.noreply.github.com>
2024-06-20 21:22:36 +0200

    align to grid function

--- src/util/math.ts
+++ src/util/math.ts
@@ -1,7 +1,15 @@
+import k from "@/kaplay";
 import { Vec2 } from "kaplay";
 
 function getMagnitute(vel: Vec2) {
   return Math.sqrt(vel.x * vel.x + vel.y * vel.y);
 }
 
-export { getMagnitute };
+function alignToGrid(vec: Vec2, stepSize: number = 64) {
+  return k.vec2(
+    Math.floor(vec.x / stepSize) * stepSize,
+    Math.floor(vec.y / stepSize) * stepSize
+  );
+}
+
+export { getMagnitute, alignToGrid };
