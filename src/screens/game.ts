95624ba487099e85eab085baa98b8568eb49776b
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2024-06-20 13:28:14 +0200

    Add dummy walls

--- src/screens/game.ts
+++ src/screens/game.ts
@@ -1,11 +1,14 @@
 import { createBackground, createField } from "@/components/background";
 import k from "@/kaplay";
 import { createPlayer } from "@/components/player";
+import { createWall } from "@/components/courseElements";
 
 k.scene("game", () => {
   createBackground();
   const playField = createField(15, 10);
   createPlayer(100, 100, playField);
+  createWall(400, 400, playField);
+  createWall(500, 500, playField);
 
   //registerCameraControls();
   console.log("Game Loaded!");
